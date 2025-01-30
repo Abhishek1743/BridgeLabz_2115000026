f7f1493cfbcf780f7d6adf5d58cdeb652b545e4f
Abhishek Sagar <91369299+Abhishek1743@users.noreply.github.com>
2025-01-30 21:38:04 +0530

    Updated SubstringOccurrence.java

--- Java_String/SubstringOccurrence.java
+++ Java_String/SubstringOccurrence.java
@@ -1,29 +1,27 @@
 import java.util.Scanner;
-
-public class SubstringOccurrence{
-    public static void main(String[] args) {
+class SubstringOccurrence{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the main string: ");
-        String mainString = sc.nextLine();
-
+        String mainString = sc.nextLine().toLowerCase();
         System.out.print("Enter the substring: ");
-        String substring = sc.nextLine();
-
-        // Call the method to count occurrences of the substring
-        int count = countSubstringOccurrences(mainString, substring);
-        System.out.println("The substring occurs " + count + " times.");
-        sc.close();
+        String substring = sc.nextLine().toLowerCase();
+        
+        System.out.println("The substring occurs " + countOccurrences(mainString, substring) + " times.");
     }
 
-    // Method to count occurrences of the substring in the main string
-    public static int countSubstringOccurrences(String mainString, String substring) {
+    public static int countOccurrences(String mainStr, String subStr){
         int count = 0;
-        int index = 0;
-
-        // Loop to find all occurrences of the substring
-        while ((index = mainString.indexOf(substring, index)) != -1) {
-            count++;
-            index += substring.length();  // Move the index forward to continue searching
+        for (int i=0; i <= mainStr.length() - subStr.length(); i++){
+            int j;
+            for (j=0; j < subStr.length(); j++){
+                if (mainStr.charAt(i + j) != subStr.charAt(j)){
+                    break;
+                }
+            }
+            if (j == subStr.length()){
+                count++;
+            }
         }
         return count;
     }
